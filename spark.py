c724ef0a4bc2c913826db25f752436148598e728
Faraaz Khan <75445343+faraazkhan1112@users.noreply.github.com>
2021-12-04 14:26:11 +0530

    Update spark.py

--- spark.py
+++ spark.py
@@ -6,32 +6,33 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import * 
 
 def json_data(rdd_data):
+  data2 = {}
   for i in rdd_data:
-    data1 = json.loads(i, strict = False)
-  return data1
+    data2 = json.loads(i, strict = False)
+  return data2
 
 def list_to_tuple(list1):
   return tuple(list1)
 
 def process1(rdd,count):
-  #PARSING THE JSON FILE AND EXTRACTING ITS ROWS
-  rows1 = []
-  rdd_data = rdd.collect()
-  data1 = json_data(rdd_data)
-  for i in data1.keys():
+	if not rdd.isEmpty():
+	#PARSING THE JSON FILE AND EXTRACTING ITS ROWS
+		rows1 = []
+		rdd_data = rdd.collect()
+		data1 = json_data(rdd_data)
+		for i in data1.keys():
+			x = list()
+			x.append(len(str(data1[i]['feature1'])))
+			x.append(str(data1[i]['feature0']).strip(' '))
+			x.append(str(data1[i]['feature1']).strip(' '))
+			x.append(str(data1[i]['feature2']).strip(' '))
     
-    x = list()
-    x.append(len(str(data1[i]['feature1'])))
-    x.append(str(data1[i]['feature0']).strip(' '))
-    x.append(str(data1[i]['feature1']).strip(' '))
-    x.append(str(data1[i]['feature2']).strip(' '))
-    
-    rows1.append(list_to_tuple(x))
-    
-  print("RECIEVED BATCH OF LENGTH ", len(rows1))
-  rdd2 = sc.parallelize(rows1)
-  #SVGD, BERNOULLI AND MULTINOMIAL CLASSIFIERS TO BE USED
-  #print("COMPLETED \n \n")
+			rows1.append(list_to_tuple(x))
+		print("RECIEVED BATCH OF LENGTH ", len(rows1))
+		#print(rows1)
+		rdd2 = sc.parallelize(rows1)
+		#SVGD, BERNOULLI AND MULTINOMIAL CLASSIFIERS TO BE USED
+		print("COMPLETED \n \n")
             
 conf = SparkConf()
 conf.setAppName("BD")
@@ -47,5 +48,4 @@ lines = dataStream.flatMap(lambda line: line.split("\n"))
 count = 1
 lines.foreachRDD(lambda rdd : process1(rdd,count))
 ssc.start()
-ssc.awaitTermination(100)
-ssc.stop()
+ssc.awaitTermination()
